b52b3e146ded2ea5cb9cfb0761bf6921ed949a45
crispycret <bnadeau.dev@gmail.com>
2022-10-28 20:26:14 -0400

    Added Procfile and gunicorn library for production

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
 
-
-
+from github import Github
 from core import app
+from config import Configuration
+
+import os
+
+port = int(os.environ.get("PORT", 5000))
 
 
-if __name__ == '__main__':
-    app.run(debug=True, port=5000)
-    
-    
-    
-    
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=port, debug=True)
+
     
     
